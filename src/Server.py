912c942eef63dc10bb5f7a75a3d766251102b94d
Sayyed Muhammad Jaffry <muhammadj110@gmail.com>
2024-06-16 05:36:15 +0330

    ServerClass implemented

---
+++ src/Server.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+import logging
+
+
+class ServerClass:
+    def __init__(self, name, host, port):
+        self.host = host
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket = None
+        self.name = name
+        logging.basicConfig(level=logging.DEBUG, format=f'%(asctime)s - {name} -- %(levelname)s - %(message)s')
+
+    def start_server(self):
+        host = self.host
+        port = self.port
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server_socket.bind((host, port))
+            server_socket.listen(5)
+            logging.info(msg=f"{self.name} server listening on port {port}...")
+
+            while True:
+                client_socket, client_address = server_socket.accept()
+                logging.info(f"Connection from {client_address}")
+                threading.Thread(target=self.handle_client, args=(client_socket,)).start()
+
+    def handle_client(self):
+        pass
+
+
